22a9958330a329f1d108945b8f2d15bd20a5ef92
chhshi <chhshi1010@gmail.com>
2018-08-23 23:26:11 -0700

    Spring #2beans of same type - @Primary

--- src/main/java/com/in28minutes/spring/basics/springin5steps/QuickSortAlgorithm.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -1,5 +1,10 @@
 package com.in28minutes.spring.basics.springin5steps;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+//[DEBUG::Parameter 0 of constructor in ...BinarySearchImpl required a single bean, but 2 were found, use  @Primary, or @Qualifier ]
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm{
   public int[] sort(int[] nums) {
 
